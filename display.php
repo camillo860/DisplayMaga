0cd544f486ce5798e0c1224490adf72a1b9cd156
camillo860 <camillo860@hotmail.com>
2023-07-24 12:06:31 +0200

    display

--- display.php
+++ display.php
@@ -1,7 +1,25 @@
-<?php
-function connectToDatabase(){
+<html>
+    <head><title> ISISS  MAGAROTTO
+</title>
+</head>
+<body>
+ <?php
+  include "utility.php";
+  echo "CIRCOLARI";
+  $result=circolariSede("Roma");
+  print $result;
+  print "<br>";
+  echo "Notizie ROMA";
 
-    
-}
+  $result=notizieSede("Roma");
+  print $result;
+  
+  print "<br>";
+  echo "Notizie NAZIONALE";
 
-?>
+  $result=notizieSede("Nazionale");
+  print $result;
+ ?>
+</body>
+
+</html>
